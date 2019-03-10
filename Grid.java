3fbe5624ea7e9d9763078093e61e600679981e48
James Tan <jamestancheemin98@gmail.com>
2019-03-11 01:40:16 +0800

    added policy iteration, pending review

--- Grid.java
+++ Grid.java
@@ -9,6 +9,7 @@ public class Grid{
     static final double ERROR = 0.01;
     static boolean converge = false;
     static final Boolean DEBUG = false;
+    static boolean value_iteration = false;
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
@@ -35,8 +36,10 @@ public class Grid{
                             + "|========================================================|\n"
                             + "||     1: Preload grid                                   ||\n"
                             + "||     2: Modify grid                                   ||\n"
-                            + "||     3: Print  grid                                   ||\n"
-                            + "||     4: Value Iteration                               ||\n"                                
+                            + "||     3: Value Iteration                               ||\n"   
+                            + "||     4: Policy Iteration                              ||\n"  
+                            + "||     5: Print grid utility                            ||\n"
+                            + "||     6: Print grid utility                            ||\n"                               
                             + "||    14: Quit                                          ||\n"
                             + "|========================================================|\n");
             System.out.printf("Enter your action: ");
@@ -72,30 +75,7 @@ public class Grid{
                     grid.get(4).get(4).set_type(Type.ORANGE);
                     break;
                 }
-                case 2  :{
-                    // while(true){
-                    //     System.out.print("Enter row of the cell (0 is top) : ");
-                    //     position[0] = sc.nextInt();
-                    //     sc.nextLine();
-                    //     if(position[0] >= 0 && position[0] < SIZE) break;
-                    //     System.out.printf("Please enter a valid coordinate between 0 and %d\n", SIZE);
-                    // }
-                    // while(true){
-                    //     System.out.print("Enter column of the cell (0 is left) : ");
-                    //     position[1] = sc.nextInt();
-                    //     sc.nextLine();
-                    //     if(position[1] >= 0 && position[1] < SIZE) break;
-                    //     System.out.printf("Please enter a valid coordinate between 0 and %d\n", SIZE);
-                    // }
-                    // while(true){
-                    //     System.out.print("Enter type of cell (GREEN, WHITE, WALL, ORANGE): ");
-                    //     try{
-                    //         type = Type.valueOf(sc.nextLine().toUpperCase());
-                    //         if(type == Type.GREEN || type == Type.WALL || type == Type.ORANGE || type == Type.WHITE) break;
-                    //     } catch (Exception e){
-                    //         System.out.println("Please enter a correct type ");
-                    //     }
-                    // }             
+                case 2  :{          
                     for(int i = 0; i<SIZE; i++){
                         for(int j = 0; j<SIZE; j++){
                             System.out.printf("Updating cell row:%d, col: %d", i , j );
@@ -116,9 +96,27 @@ public class Grid{
 
                     break;
                 }
-                case 3  : {
-                    System.out.println("Printing grid:");
-                    print_grid(grid);
+                case 3  :{
+                    System.out.print("Enter number of iterations:");
+                    total_count = sc.nextInt();
+                    sc.nextLine();
+                    int count;
+                    long start_time = System.currentTimeMillis();
+                    value_iteration = true;
+                    converge = false;
+                    for(count = 0; count<total_count; count++){
+                        if(converge) break;
+                        converge = true;
+                        iterate_grid(grid);
+                        if(DEBUG){
+                            System.out.printf("-----------------------ITERATION #%d --------------------------", count);
+                            print_util(grid);
+                        }
+                    }
+                    print_util(grid);
+                    print_policy(grid);
+                    long duration = System.currentTimeMillis()-start_time;
+                    System.out.printf("Value Iteration took %d iterations\nElapsed for a total of %d.%d seconds\n",count,duration/1000,duration%1000);
                     break;
                 }
                 case 4  :{
@@ -127,19 +125,31 @@ public class Grid{
                     sc.nextLine();
                     int count;
                     long start_time = System.currentTimeMillis();
+                    value_iteration = false;
                     converge = false;
                     for(count = 0; count<total_count; count++){
                         if(converge) break;
                         converge = true;
-                        value_iteration(grid);
+                        iterate_grid(grid);
                         if(DEBUG){
                             System.out.printf("-----------------------ITERATION #%d --------------------------", count);
-                            print_grid(grid);
+                            print_util(grid);
                         }
                     }
-                    print_grid(grid);
+                    print_util(grid);
+                    print_policy(grid);
                     long duration = System.currentTimeMillis()-start_time;
                     System.out.printf("Value Iteration took %d iterations\nElapsed for a total of %d.%d seconds\n",count,duration/1000,duration%1000);
+                    break;
+                }
+                case 5  : {
+                    System.out.println("Printing grid utility:");
+                    print_util(grid);
+                    break;
+                }
+                case 6  : {
+                    System.out.println("Printing grid policy:");
+                    print_policy(grid);
                     break;
                 }
                 default :{
@@ -162,32 +172,37 @@ public class Grid{
         return grid.get(position[0]).get(position[1]);
     }
 
-    public static void value_iteration(ArrayList<ArrayList<Cell>> grid){
+    public static void iterate_grid(ArrayList<ArrayList<Cell>> grid){
         ArrayList<ArrayList<Double>> grid_util = new ArrayList<ArrayList<Double>>();
+        ArrayList<ArrayList<Choice>> grid_policy = new ArrayList<ArrayList<Choice>>();
         int[] position = new int[2];
         double new_util;
 
         for(int i=0; i<SIZE; i++){
             grid_util.add(new ArrayList<Double>());
+            grid_policy.add(new ArrayList<Choice>());
         }
 
         for(ArrayList<Cell> row: grid){
             for(Cell selected: row){
                 position[0] = selected.get_row();
                 position[1] = selected.get_col();
-                new_util = get_utility(grid, position);
+                new_util = get_utility(grid, grid_policy, position);
                 grid_util.get(position[0]).add(new_util);
             }
         }
         update_grid(grid, grid_util);
+        update_policy(grid, grid_policy);
     }
 
-    public static double get_utility(ArrayList<ArrayList<Cell>> grid, int[] position){
+    public static double get_utility(ArrayList<ArrayList<Cell>> grid, ArrayList<ArrayList<Choice>> grid_policy, int[] position){
         double util_up=0, util_down=0, util_left=0, util_right=0;
-        double weighted_util_up=0, weighted_util_down=0, weighted_util_left=0, weighted_util_right=0;
+        double weighted_util_left=0, weighted_util_right=0, weighted_util_up=0, weighted_util_down=0, weighted_util_choice=0;
 
-        if(get_cell(grid, position).is_wall()) return 0;
-
+        if(get_cell(grid, position).is_wall()) {
+            grid_policy.get(position[0]).add(Choice.WALL);
+            return 0;
+        }
         if(position[0] == 0) util_up = get_cell(grid,position).get_util();
         else if(grid.get(position[0]-1).get(position[1]).is_wall()) util_up = get_cell(grid,position).get_util();
         else{
@@ -220,29 +235,57 @@ public class Grid{
             position[1]--;
         }
 
+        weighted_util_up = STRAIGHT_PROB*util_up + (1-STRAIGHT_PROB)*(util_left+util_right)/2;
         weighted_util_down = STRAIGHT_PROB*util_down + (1-STRAIGHT_PROB)*(util_left+util_right)/2;
         weighted_util_left = STRAIGHT_PROB*util_left + (1-STRAIGHT_PROB)*(util_up+util_down)/2;
         weighted_util_right = STRAIGHT_PROB*util_right + (1-STRAIGHT_PROB)*(util_up+util_down)/2;
-        weighted_util_up = STRAIGHT_PROB*util_up + (1-STRAIGHT_PROB)*(util_left+util_right)/2;
+        weighted_util_choice = Math.max(Math.max(weighted_util_down, weighted_util_left),Math.max(weighted_util_up, weighted_util_right));
+        
+        if(weighted_util_choice == weighted_util_up) grid_policy.get(position[0]).add(Choice.UP);
+        else if(weighted_util_choice == weighted_util_down) grid_policy.get(position[0]).add(Choice.DOWN);
+        else if(weighted_util_choice == weighted_util_left) grid_policy.get(position[0]).add(Choice.LEFT);
+        else grid_policy.get(position[0]).add(Choice.RIGHT);
 
-        return Math.max(Math.max(weighted_util_down, weighted_util_left),Math.max(weighted_util_up, weighted_util_right))*DISCOUNT + get_cell(grid,position).get_reward();
+        return weighted_util_choice*DISCOUNT + get_cell(grid,position).get_reward();
     }
 
     public static void update_grid(ArrayList<ArrayList<Cell>> grid, ArrayList<ArrayList<Double>> grid_util){
         for(int i = 0; i< SIZE; i++){
             for(int j =0; j< SIZE; j++){
-                if(Math.abs(grid.get(i).get(j).get_util()-grid_util.get(i).get(j)) > ERROR) converge = false;
+                if(Math.abs(grid.get(i).get(j).get_util()-grid_util.get(i).get(j)) > ERROR && value_iteration == true) converge = false;
                 grid.get(i).get(j).set_util(grid_util.get(i).get(j));
             }
         }
     }
 
-    public static void print_grid(ArrayList<ArrayList<Cell>> grid){
+    public static void update_policy(ArrayList<ArrayList<Cell>> grid, ArrayList<ArrayList<Choice>> grid_policy){
+        for(int i = 0; i< SIZE; i++){
+            for(int j =0; j< SIZE; j++){
+                if(grid.get(i).get(j).get_policy()!=grid_policy.get(i).get(j) && value_iteration == false) converge = false;
+                grid.get(i).get(j).set_policy(grid_policy.get(i).get(j));
+            }
+        }
+    }
+
+    public static void print_util(ArrayList<ArrayList<Cell>> grid){
         System.out.println("-------------------------------------------------------");
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid.get(i).size(); j++){
                 System.out.print("| ");
                 System.out.print(grid.get(i).get(j));
+                System.out.print(" |");
+            }
+            System.out.println("");
+            System.out.println("-------------------------------------------------------");
+        }
+    }
+
+    public static void print_policy(ArrayList<ArrayList<Cell>> grid){
+        System.out.println("-------------------------------------------------------");
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid.get(i).size(); j++){
+                System.out.print("| ");
+                System.out.printf("%5s", grid.get(i).get(j).get_policy());
                 System.out.print(" |");
             }
             System.out.println("");
